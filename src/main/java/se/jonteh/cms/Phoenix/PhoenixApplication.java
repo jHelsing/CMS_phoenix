c98bbccd09b2cc5b1b1099d51051fae984d20d1b
Jonathan Helsing <jonathan.helsing@gmail.com>
2020-06-07 19:35:03 +0200

    Initial project content

---
+++ src/main/java/se/jonteh/cms/Phoenix/PhoenixApplication.java
@@ -0,0 +1,13 @@
+package se.jonteh.cms.Phoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PhoenixApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/jonteh/cms/Phoenix/PhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jonteh.cms.Phoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
